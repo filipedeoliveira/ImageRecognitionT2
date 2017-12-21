a5fd8a32ab4ac7454bf9ac5f00665b53793b7462
Stéphane <stephanefernandes@hotmail.com>
2017-12-21 14:37:06 +0000

    rodas verdes a volta das moedas

--- main_image_recognition.m
+++ main_image_recognition.m
@@ -3,6 +3,9 @@ path = fullfile(mfilename('class'),'output',image);
 src_image =imread(path);
 path_output = fullfile(mfilename('class'),'output');
 image_name = image(1:end-4);
+warning('off', 'Images:initSize:adjustingMag');
+captionFontSize = 14;
+
 
 %------------------------------------FUNÇÃO 1  ruido----------------------------------------
 %Your program should ask to the user the type of noise to be added separately to the input
@@ -38,7 +41,7 @@ image_name = image(1:end-4);
 %--------------------FAZER--------------------------------
 %SNR of the noisy image to verify the level of noise introduced;
 
-    %linha 18  e 33
+    %feito mais abaixo
 %------------------------------------FUNÇÃO 2 fazer pré prossecamento ----------------------------------------
 %1- filtering methods
 
@@ -61,53 +64,79 @@ elseif(n==2)
     figure
     subplot(1,2,1), imshow(image_gauss), title('Image Gaussian noise');
     subplot(1,2,2), imshow(image_gaussian_filter), title('Image Filtered using Gaussian Filter');
-end 
+end
+
 %Contrast equalization
 if(n==1) image_to_use = image_median_filter;
 elseif(n==2) image_to_use = image_gaussian_filter;
 end
-%usando função imadjust
-image_contrast = imadjust(image_to_use);
-figure
-subplot(1,2,1), imshow(image_to_use), title('Image Filtered');
-subplot(1,2,2), imshow(image_contrast), title('Contrast adjusted using function "imadjust"');
-% usando Histogram Equalization
-Hist = histeq(image_to_use);
+% 2 Histogram Equalization
+Imagem_contraste = histeq(image_to_use);
 
 figure;
 subplot(2,2,1), imshow(image_to_use);
 title('Image Filtered');
 subplot(2,2,2), imhist(image_to_use,64);
 title('Image Filtered Histogram');
-subplot(2,2,3), imshow(Hist);
+subplot(2,2,3), imshow(Imagem_contraste);
 title('Image after histogram equalization');
-subplot(2,2,4), imhist(Hist,64);
-title('Image after histogram equalization Histogram');
+subplot(2,2,4), imhist(Imagem_contraste,64);
+title('Histogram equalization ');
+
+% 3 - Normalizar 
+Imagem_normalizada = mat2gray(Imagem_contraste);
+figure;
+imshow(Imagem_normalizada);
+title('Imagem normalizda');
+
+%4 threshold the image to get a binary image (only 0's and 1's)using im2bw()
+     normalizedThresholdValue = 0.45; % In range 0 to 1.
+      thresholdValue = normalizedThresholdValue * max(max(Imagem_normalizada)); 
+      binaryImage = im2bw(Imagem_normalizada, normalizedThresholdValue);       
+      binaryImage = Imagem_normalizada < thresholdValue; 
+      binaryImage = imfill(binaryImage, 'holes');
+      figure;
+      imshow(binaryImage);
+      title('binary');
+
+      labeledImage = bwlabel(binaryImage, 8);  
+      tamanhoMoeadas = regionprops(labeledImage, Imagem_normalizada, 'all');
+      numberOfBlobs = size(tamanhoMoeadas, 1);
+
+figure;
+imshow(Imagem_normalizada);
+title('Outlines, from bwboundaries()', 'FontSize', captionFontSize); 
+axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+hold on;
+boundaries = bwboundaries(binaryImage);
+numberOfBoundaries = size(boundaries, 1);
+for k = 1 : numberOfBoundaries
+	thisBoundary = boundaries{k};
+	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
+end
+hold off;
 
 
+  
 %------------------------------------FUNÇÃO 3 segmentação ----------------------------------------
 %use a sequence of functions that solves the task of segmenting all of the coins in the image
-image_to_use = Hist;
-%Prewitt
-BW1 = edge(image_to_use,'Prewitt');
-%Sobel
-BW2 = edge(image_to_use,'Sobel');
-%canny 
-BW3 = edge(image_to_use,'Canny');
-figure;
-subplot(2,2,1), imshow(BW1);
-title('Prewitt');
-subplot(2,2,2), imshow(BW2);
-title('Sobel');
-subplot(2,2,3), imshow(BW3);
-title('Canny');
-subplot(2,2,4), imshow(image_to_use);
-title('Original');
 
-Hough(image_to_use,10,7,5);
+%Otsu's fazer
 
+%K-MEANS - não esta a funcionar
+%img_as_col = double(Imagem_normalizada(:));
+%cluster_menbs = kmeans(img_as_col, 2,'distance', 'sqeuclidean');
+%labelim = zeros(size(img_as_col));
+%for i=1:3
+%    inds = find(cluster_menbs==i);
+%    meanval = mean(img_as_col(inds));
+%    labelim(inds) = meanval;
+%end
+%figure
+%imshow(labelim, []);
 
-HoughTransformCircles(image_to_use);
+%Hough(image_to_use,10,7,5);
+%HoughTransformCircles(src_image);
 
 
 %------------------------------------FUNÇÃO 4 total de moedas ----------------------------------------
@@ -124,4 +153,5 @@ HoughTransformCircles(image_to_use);
 %implement an algorithm that count the total amount of money.
 
 end
+
 
